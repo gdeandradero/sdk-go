76eac57fbcf54d5df87d5c72970633ebdd97121f
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2023-09-12 18:53:49 -0300

    Add cancel, capture and capture amount methods

--- pkg/payment/payment.go
+++ pkg/payment/payment.go
@@ -15,6 +15,7 @@ const (
 	postURL   = "https://api.mercadopago.com/v1/payments"
 	searchURL = "https://api.mercadopago.com/v1/payments/search"
 	getURL    = "https://api.mercadopago.com/v1/payments/{id}"
+	putURL    = "https://api.mercadopago.com/v1/payments/{id}"
 )
 
 // Client contains the methods to interact with the Payments API.
@@ -24,21 +25,39 @@ type Client interface {
 		It is a post request to the endpoint: https://api.mercadopago.com/v1/payments
 		Reference: https://www.mercadopago.com.br/developers/pt/reference/payments/_payments/post/
 	*/
-	Create(dto Request) (*Response, error)
+	Create(dto Request, opts ...rest.Option) (*Response, error)
 
 	/*
 		Search searches for payments.
 		It is a get request to the endpoint: https://api.mercadopago.com/v1/payments/search
 		Reference: https://www.mercadopago.com.br/developers/pt/reference/payments/_payments_search/get/
 	*/
-	Search(f Filters) (*SearchResponse, error)
+	Search(f Filters, opts ...rest.Option) (*SearchResponse, error)
 
 	/*
 		Get gets a payment by its ID.
 		It is a get request to the endpoint: https://api.mercadopago.com/v1/payments/{id}
 		Reference: https://www.mercadopago.com.br/developers/pt/reference/payments/_payments_id/get/
 	*/
-	Get(id int64) (*Response, error)
+	Get(id int64, opts ...rest.Option) (*Response, error)
+
+	/*
+		Cancel cancels a payment by its ID.
+		It is a put request to the endpoint: https://api.mercadopago.com/v1/payments/{id}
+	*/
+	Cancel(id int64, opts ...rest.Option) (*Response, error)
+
+	/*
+		Capture captures a payment by its ID.
+		It is a put request to the endpoint: https://api.mercadopago.com/v1/payments/{id}
+	*/
+	Capture(id int64, opts ...rest.Option) (*Response, error)
+
+	/*
+		CaptureAmount captures amount of a payment by its ID.
+		It is a put request to the endpoint: https://api.mercadopago.com/v1/payments/{id}
+	*/
+	CaptureAmount(id int64, amount float64, opts ...rest.Option) (*Response, error)
 }
 
 // client is the implementation of Client.
@@ -51,7 +70,7 @@ func NewClient() Client {
 	return &client{hc: rest.Instance()}
 }
 
-func (c *client) Create(dto Request) (*Response, error) {
+func (c *client) Create(dto Request, opts ...rest.Option) (*Response, error) {
 	body, err := json.Marshal(&dto)
 	if err != nil {
 		return nil, err
@@ -63,7 +82,7 @@ func (c *client) Create(dto Request) (*Response, error) {
 		return nil, err
 	}
 
-	res, err := c.hc.Send(req)
+	res, err := c.hc.Send(req, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -90,7 +109,7 @@ func (c *client) Create(dto Request) (*Response, error) {
 	return formatted, nil
 }
 
-func (c *client) Search(f Filters) (*SearchResponse, error) {
+func (c *client) Search(f Filters, opts ...rest.Option) (*SearchResponse, error) {
 	params := url.Values{}
 	params.Add("sort", f.Sort)
 	params.Add("criteria", f.Criteria)
@@ -104,7 +123,7 @@ func (c *client) Search(f Filters) (*SearchResponse, error) {
 		return nil, err
 	}
 
-	res, err := c.hc.Send(req)
+	res, err := c.hc.Send(req, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -131,7 +150,7 @@ func (c *client) Search(f Filters) (*SearchResponse, error) {
 	return formatted, nil
 }
 
-func (c *client) Get(id int64) (*Response, error) {
+func (c *client) Get(id int64, opts ...rest.Option) (*Response, error) {
 	conv := strconv.Itoa(int(id))
 	url := strings.Replace(getURL, "{id}", conv, 1)
 	req, err := http.NewRequest("GET", url, nil)
@@ -139,7 +158,133 @@ func (c *client) Get(id int64) (*Response, error) {
 		return nil, err
 	}
 
-	res, err := c.hc.Send(req)
+	res, err := c.hc.Send(req, opts...)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	read, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, &rest.ErrorResponse{
+			StatusCode: res.StatusCode,
+			Message:    string(read),
+			Headers:    res.Header,
+		}
+	}
+
+	formatted := &Response{}
+	if err := json.Unmarshal(read, &formatted); err != nil {
+		return nil, err
+	}
+
+	return formatted, nil
+}
+
+func (c *client) Cancel(id int64, opts ...rest.Option) (*Response, error) {
+	dto := &CancelRequest{Status: "cancelled"}
+	body, err := json.Marshal(dto)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := strings.NewReader(string(body))
+	conv := strconv.Itoa(int(id))
+	url := strings.Replace(putURL, "{id}", conv, 1)
+	req, err := http.NewRequest("PUT", url, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.hc.Send(req, opts...)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	read, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, &rest.ErrorResponse{
+			StatusCode: res.StatusCode,
+			Message:    string(read),
+			Headers:    res.Header,
+		}
+	}
+
+	formatted := &Response{}
+	if err := json.Unmarshal(read, &formatted); err != nil {
+		return nil, err
+	}
+
+	return formatted, nil
+}
+
+func (c *client) Capture(id int64, opts ...rest.Option) (*Response, error) {
+	dto := &CaptureRequest{Capture: true}
+	body, err := json.Marshal(dto)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := strings.NewReader(string(body))
+	conv := strconv.Itoa(int(id))
+	url := strings.Replace(putURL, "{id}", conv, 1)
+	req, err := http.NewRequest("PUT", url, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.hc.Send(req, opts...)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	read, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, &rest.ErrorResponse{
+			StatusCode: res.StatusCode,
+			Message:    string(read),
+			Headers:    res.Header,
+		}
+	}
+
+	formatted := &Response{}
+	if err := json.Unmarshal(read, &formatted); err != nil {
+		return nil, err
+	}
+
+	return formatted, nil
+}
+
+func (c *client) CaptureAmount(id int64, amount float64, opts ...rest.Option) (*Response, error) {
+	dto := &CaptureRequest{TransactionAmount: amount, Capture: true}
+	body, err := json.Marshal(dto)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := strings.NewReader(string(body))
+	conv := strconv.Itoa(int(id))
+	url := strings.Replace(putURL, "{id}", conv, 1)
+	req, err := http.NewRequest("PUT", url, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.hc.Send(req, opts...)
 	if err != nil {
 		return nil, err
 	}
